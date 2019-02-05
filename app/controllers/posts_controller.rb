8e1863f4bed368052de0b56f0eca1320b01ad822
Rudy Ravelin <rudy@noukod.com>
2019-02-05 15:50:57 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,50 @@
-class PostsController < ApplicationController
+# class PostsController < ApplicationController
+#
+#   def index
+#     @posts = Post.all
+#   end
+#
+#   def show
+#     @post = Post.find(params[:id])
+#   end
+#
+#   def new
+#     @post = Post.new
+#   end
+#
+#   def create
+#     @post = Post.new(post_params)
+#     @post.save
+#     redirect_to post_path(@post)
+#   end
+#
+#   def update
+#     @post = Post.find(params[:id])
+#     @post.update(post_params)
+#     redirect_to post_path(@post)
+#   end
+#
+#   def edit
+#     @post = Post.find(params[:id])
+#   end
+#
+# private
+#
+#   def post_params
+#     params.require(:post).permit(:title, :desription, :post_status, :author_id)
+#   end
+# end
+
+
 
   def index
-    @posts = Post.all
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
   end
 
   def show
     @post = Post.find(params[:id])
   end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to post_path(@post)
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to post_path(@post)
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-private
-
-  def post_params
-    params.require(:post).permit(:title, :desription, :post_status, :author_id)
-  end
-end
