f7fcc24dd5f3531f94b1acd803525ccf77b9f847
Rudy Ravelin <rudy@noukod.com>
2019-02-05 15:48:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,35 @@
 # class PostsController < ApplicationController
-# 
+#
 #   def index
 #     @posts = Post.all
 #   end
-# 
+#
 #   def show
 #     @post = Post.find(params[:id])
 #   end
-# 
+#
 #   def new
 #     @post = Post.new
 #   end
-# 
+#
 #   def create
 #     @post = Post.new(post_params)
 #     @post.save
 #     redirect_to post_path(@post)
 #   end
-# 
+#
 #   def update
 #     @post = Post.find(params[:id])
 #     @post.update(post_params)
 #     redirect_to post_path(@post)
 #   end
-# 
+#
 #   def edit
 #     @post = Post.find(params[:id])
 #   end
-# 
+#
 # private
-# 
+#
 #   def post_params
 #     params.require(:post).permit(:title, :desription, :post_status, :author_id)
 #   end
@@ -44,7 +44,7 @@
       @posts = Post.all
     end
   end
- 
+
   def show
     @post = Post.find(params[:id])
   end
