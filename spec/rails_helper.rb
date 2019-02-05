045a886731e5811a284e1f13ac8c4a47c00975dc
Rudy Ravelin <rudy@noukod.com>
2019-02-05 15:50:15 +0000

    Automatically backed up by Learn

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@
 # require 'rspec/rails'
 # require 'capybara/rails'
 # # Add additional requires below this line. Rails is not loaded until this point!
-# 
+#
 # # Requires supporting ruby files with custom matchers and macros, etc, in
 # # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # # run as spec files by default. This means that files in spec/support that end
@@ -22,42 +22,42 @@
 # # require only the support files necessary.
 # #
 # # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-# 
+#
 # # Checks for pending migration and applies them before tests are run.
 # # If you are not using ActiveRecord, you can remove this line.
 # ActiveRecord::Migration.maintain_test_schema!
-# 
+#
 # RSpec.configure do |config|
 #   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
 #   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-# 
+#
 #   # If you're not using ActiveRecord, or you'd prefer not to run each of your
 #   # examples within a transaction, remove the following line or assign false
 #   # instead of true.
 #   config.use_transactional_fixtures = false
-# 
+#
 #   config.before(:suite) do
 #     DatabaseCleaner.clean_with(:truncation)
 #   end
-# 
+#
 #   config.before(:each) do
 #     DatabaseCleaner.strategy = :transaction
 #   end
-# 
+#
 #   config.before(:each, :js => true) do
 #     DatabaseCleaner.strategy = :truncation
 #   end
-# 
+#
 #   config.before(:each) do
 #     DatabaseCleaner.start
 #   end
-# 
+#
 #   config.after(:each) do
 #     DatabaseCleaner.clean
 #   end
-# 
+#
 #   config.infer_spec_type_from_file_location!
-# 
+#
 #   # Filter lines from Rails gems in backtraces.
 #   config.filter_rails_from_backtrace!
 #   # arbitrary gems may also be filtered via:
